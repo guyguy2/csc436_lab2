6358e93d2b06cca80e1d3ad5e9c6846db5ee1b02
Guy <guylevi@hotmail.com>
2022-11-20 21:49:00 -0600

    express backend

---
+++ express-backend/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var app = express();
+
+require("./setupMongo")();
+require("dotenv").config();
+
+app.use(express.json());
+
+// middleware function here to append a trace header to a request
+// app.use(function (req, res, next) {
+//   req.traceId = uuidv4();
+//   next();
+// });
+
+app.use("/auth", require("./routes/auth"));
+app.use("/todo", require("./routes/todo"));
+
+module.exports = app;
